4feab1005d59cdd60ffc3561f6cf61c273131945
JavaSprite <JavaSprite.com@gmail.com>
2019-04-05 17:05:37 +0800

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,13 +18,15 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = '郑志强';
         $user->email = '779362963@qq.com';
-        $user->password = '20190222F&Q';
+        $user->password = bcrypt('20190222F&Q');
+        $user->is_admin = true;
         $user->save();
 
         $user = User::find(2);
         $user->name = '邹方圆';
         $user->email = '1436925891@qq.com';
-        $user->password = '20190222F&Q';
+        $user->password = bcrypt('20190222F&Q');
+        $user->is_admin = true;
         $user->save();
     }
 }
