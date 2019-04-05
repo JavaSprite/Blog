390a77da7c3ad9621cd57e692d94c0bc1f6f2f48
JavaSprite <JavaSprite.com@gmail.com>
2019-04-05 13:26:46 +0800

    用户退出登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -14,23 +14,30 @@
 	    <!-- Collect the nav links, forms, and other content for toggling -->
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-	    	<ul class="nav navbar-nav">
-		        <li><a href="{{ route('help') }}">帮助<span class="sr-only">(current)</span></a></li>
-		        <li><a href="#">Link</a></li>
-		    </ul>
-
 	      <ul class="nav navbar-nav navbar-right">
-	        <li><a href="#">用户</a></li>
-	        <li class="dropdown">
-	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">邹方圆<span class="caret"></span></a>
-	          <ul class="dropdown-menu">
-	            <li><a href="#">Action</a></li>
-	            <li><a href="#">Another action</a></li>
-	            <li><a href="#">Something else here</a></li>
-	            <li role="separator" class="divider"></li>
-	            <li><a href="#">Separated link</a></li>
-	          </ul>
-	        </li>
+	        @if (Auth::check())
+		        <li class="nav-item"><a class="nav-link" href="#">当前用户</a></li>
+
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}<span class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a></li>
+		            <li><a class="dropdown-item" href="#">编辑资料</a></li>
+		            <li>
+		            	<a class="dropdown-item" id="logout" href="#">
+			              <form action="{{ route('logout') }}" method="POST">
+			                {{ csrf_field() }}
+			                {{ method_field('DELETE') }}
+			                <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+			              </form>
+			            </a>
+		            </li>
+		          </ul>
+		        </li>
+		    @else
+		        <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
+		        <li class="nav-item" ><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+		    @endif
 	      </ul>
 	    </div><!-- /.navbar-collapse -->
 	  </div><!-- /.container-fluid -->
